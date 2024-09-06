646fd71d41679cbc5ab1d4699b163e3b717073cc
Chris Gutwin <24538491+cgutwin@users.noreply.github.com>
2024-09-05 23:28:39 -0700

    fish: fzf configured, z added

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,10 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-	fish_add_path /opt/homebrew/bin
+	# Commands to run in interactive sessions can go here
+	
+	fish_add_path /opt/homebrew/bin						# include items from homebrew installs
+	set -gx EDITOR "vim"							# set the default editor
+
+	set -x LS_COLORS (vivid generate rose-pine)				# set LS_COLORS env for fd
+	fzf_configure_bindings --directory=\cf 					# configures bindings for fzf
+	set fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"	# binds ctrl-o to open using the default editor
 end
